a43d0cfd5c1800fe1b6fcd90503f03b4a20ca4b8
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-11-21 22:43:04 +0530

    added server URL syraight

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const getBackendUrl = () => {
     if (process.env.NODE_ENV === "development") {
       return "http://127.0.0.1:8000";
     } else if (process.env.NODE_ENV === "production") {
-      return process.env.NEXT_PUBLIC_DJANGO_SERVER_URL;
+      return "https://task-manager-server-m05d.onrender.com";
     } else {
       return "http://127.0.0.1:8000"; 
     }
